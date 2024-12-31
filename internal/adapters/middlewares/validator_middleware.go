e50705f62ebf10336b1914402a305ccd1cc59bd9
SornchaiTheDev <sornchaithedev@gmail.com>
2024-12-31 16:15:00 +0700

    feat(middlware): validate argument that pass to the function

--- internal/adapters/middlewares/validator_middleware.go
+++ internal/adapters/middlewares/validator_middleware.go
@@ -1,11 +1,23 @@
 package middlewares
 
 import (
+	"errors"
+	"reflect"
+
 	"github.com/SornchaiTheDev/cs-lab-backend/internal/validator"
 	"github.com/gofiber/fiber/v2"
 )
 
 func ValidateMiddleware(r any) func(*fiber.Ctx) error {
+	if err := isStructPointer(r); err != nil {
+		return func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error":   "Internal Server Error",
+				"message": "Invalid using ValidateMiddleware need to pass struct pointer to the middlware",
+			})
+		}
+	}
+
 	appValidator := validator.NewAppValidator()
 
 	return func(c *fiber.Ctx) error {
@@ -22,4 +34,13 @@ func ValidateMiddleware(r any) func(*fiber.Ctx) error {
 
 		return c.Next()
 	}
+}
+
+func isStructPointer(i any) error {
+	typ := reflect.TypeOf(i)
+	if typ.Kind() != reflect.Ptr {
+		return errors.New("not a pointer")
+	}
+
+	return nil
 }
